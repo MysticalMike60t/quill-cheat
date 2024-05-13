4100223696268e431a87afa5f14de1836256addb
MysticalMike60t <caden.finkelstein@cttech.org>
2024-05-13 08:07:58 -0400

    Base Commit

---
+++ code.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         Website Response Grabber
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Grab response from a website
+// @author       You
+// @match        https://www.quill.org/connect/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  function getUrlParams(url) {
+    const params = new URLSearchParams(url);
+    const studentId = params.get("student");
+    return { studentId };
+  }
+
+  function getQuestionId(jsonData) {
+    return jsonData.currentQuestion.question;
+  }
+
+  const currentUrl = window.location.href;
+
+  const { studentId } = getUrlParams(currentUrl);
+  const jsonUrl = `https://www.quill.org/api/v1/active_activity_sessions/${studentId}.json`;
+
+  fetch(jsonUrl)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      const questionId = getQuestionId(jsonData);
+
+      const apiUrl = `https://cms.quill.org/questions/${questionId}/responses`;
+
+      fetch(apiUrl)
+        .then((response) => response.text())
+        .then((data) => {
+          console.log("Response:", data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    })
+    .catch((error) => {
+      console.error("Error fetching JSON data:", error);
+    });
+})();
